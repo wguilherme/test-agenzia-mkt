9f20ad7201022d80092e8b12c9583c1fd76c33c2
Withney Guilherme <wguilhermecriativo@gmail.com>
2022-07-08 19:16:01 -0300

    feat: add backbutton atom

---
+++ frontend/src/components/atom/BackButton/BackButton.tsx
@@ -0,0 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+import { IconButton } from "@mui/material";
+import { ArrowBackIos } from '@mui/icons-material';
+
+type PropsInterface = {
+  show?: boolean
+}
+
+export const BackButton = ({show}: PropsInterface) => {
+  const navigate = useNavigate();
+  return (
+    <IconButton  edge="start" onClick={() => navigate(-1)} sx={{ pl: 2 }}>
+      <ArrowBackIos sx={{
+        color: 'primary.contrastText',
+        visibility: show ? 'visibile' : 'hidden'
+        }} color="primary"  />
+    </IconButton>
+  )
+}
