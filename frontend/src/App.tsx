7ec967856e8eef601d27ed3f4683ef1566bac5b5
Withney Guilherme <wguilhermecriativo@gmail.com>
2022-07-08 11:22:12 -0300

    chore: config material UI

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import {Home} from '@/pages';
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
+export default App
---
+++ frontend/src/main.tsx
@@ -0,0 +1,40 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { indigo } from '@mui/material/colors';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import { LoggedUserProvider } from './contexts/User';
+import { store } from './store';
+import './styles/global.scss';
+
+const theme: any = createTheme({
+  palette: {
+    // mode: 'dark',
+    primary: indigo
+  },
+  typography: {
+    fontFamily: 'Roboto',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  }
+})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <LoggedUserProvider>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </LoggedUserProvider>
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
